498fcade94c09702534a1a0a566bc38e4beafbcb
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-06-26 00:27:39 +0900

    prettierを追加

--- typescript-space/src/declaration-merging.ts
+++ typescript-space/src/declaration-merging.ts
@@ -1,24 +1,24 @@
 interface Fish {
-  name: string;
+  name: string
 }
 
 interface Fish {
-  weight: number;
-  height: number;
+  weight: number
+  height: number
 }
 
-type Name = { name: string };
+type Name = { name: string }
 
 type Size = {
-  weight: number;
-  height: number;
-};
+  weight: number
+  height: number
+}
 
 // type Fish = Name & Size
 
-const salmon: Fish = {
-  name: "Salmon",
-};
+// const salmon: Fish = {
+//   name: "Salmon",
+// };
 
 //interface　のときのエラー
 //Type '{ name: string; }' is missing the following properties from type 'Fish': weight, height ts(2739)
@@ -28,14 +28,13 @@ const salmon: Fish = {
 //Type '{ name: string; }' is missing the following properties from type 'Size': weight, height ts(2322)
 
 interface Octopus {
-  name: string;
+  name: string
 }
 
-interface Octopus {
-  name: "octopus";
-}
+// interface Octopus {
+//   name: "octopus";
+// }
 //Subsequent property declarations must have the same type.  Property 'name' must be of type 'string', but here has type '"octopus"'.ts(2717)
-
 
 // interface Tuna {
 //   weight: number;
@@ -47,8 +46,6 @@ interface Octopus {
 //   swim(velocity: number): number;
 // }
 
-
-
 // const tuna: Tuna = {
 //   name: "Tuna",
 //   weight: 40,
@@ -56,16 +53,15 @@ interface Octopus {
 //   swim: (velocity) => velocity, // (method) Tuna.swim(velocity: 123): number (+1 overload)
 // };
 
-
 interface Tuna {
-  name: string;
-  swim(velocity: number): number;
+  name: string
+  swim(velocity: number): number
 }
 
 interface Tuna {
-  weight: number;
-  height: number;
-  swim(velocity: 123): number;
+  weight: number
+  height: number
+  swim(velocity: 123): number
 }
 
 const tuna: Tuna = {
@@ -73,6 +69,6 @@ const tuna: Tuna = {
   weight: 40,
   height: 80,
   swim: (velocity) => velocity, // (method) Tuna.swim(velocity: number): number (+1 overload)
-};
+}
 
-export {};
+export {}
