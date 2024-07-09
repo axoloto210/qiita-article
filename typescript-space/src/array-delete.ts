f823a34d0e8ee57cedfb12567d50520c6ecf4362
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-07-09 09:14:47 +0900

    配列の要素削除のコード例を追加

---
+++ typescript-space/src/array-delete.ts
@@ -0,0 +1,26 @@
+// const array = [1, 2, 3]
+
+// const newArray = array.filter((element)=> {
+//     return true
+// })
+
+// console.log(newArray)
+// // [ 1, 2, 3 ]
+
+// console.log(Object.is(newArray, array))
+// // false
+
+const array = [1, 2, 3]
+
+const poppedElement = array.pop()
+
+console.log(poppedElement)
+// 3
+
+console.log(array)
+// [ 1, 2 ]
+const empty = [].pop()
+
+console.log(empty)
+// undefined
+export {}
