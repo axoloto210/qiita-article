a811d322edde015af1bec235c85adcc7679af91e
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-07-09 20:13:25 +0900

    splice の例を追加

--- typescript-space/src/array-delete.ts
+++ typescript-space/src/array-delete.ts
@@ -10,17 +10,37 @@
 // console.log(Object.is(newArray, array))
 // // false
 
-const array = [1, 2, 3]
+// const array = [1, 2, 3]
 
-const poppedElement = array.pop()
+// const poppedElement = array.pop()
 
-console.log(poppedElement)
-// 3
+// console.log(poppedElement)
+// // 3
+
+// console.log(array)
+// // [ 1, 2 ]
+// const empty = [].pop()
+
+// console.log(empty)
+// // undefined
+
+// const array = ['a', 'b', 'c', 'd']
+// array.splice(1, 2, 'e', 'f')
+
+// console.log(array)
+// //[ 'a', 'e', 'f', 'd' ]
+
+// const array = ['a', 'b', 'c', 'd']
+// array.splice(2, Infinity)
+
+// console.log(array)
+// // [ 'a', 'b' ]
+
+
+const array = ['a', 'b', 'c', 'd', 'e', 'f']
+array.splice(2, 3, 'g')
 
 console.log(array)
-// [ 1, 2 ]
-const empty = [].pop()
+//[ 'a', 'b', 'g', 'f' ]
 
-console.log(empty)
-// undefined
 export {}
