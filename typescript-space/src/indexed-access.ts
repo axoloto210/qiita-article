94effa7f87734a926dcd8acab1959d9943a2ec12
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-07-11 22:14:55 +0900

    indexed access のコード例を追加

---
+++ typescript-space/src/indexed-access.ts
@@ -0,0 +1,15 @@
+
+  type Fish = { name?: string; age?: number }
+
+  let fishName: keyof Fish = "name"
+
+  const fish: Fish =
+    Math.random() > 0.5 ? { name: "salmon" } : { name: undefined }
+
+  if (typeof fish[fishName] !== "undefined") {
+    fish[fishName].toUpperCase() // Object is possibly 'undefined'.ts(2532)
+    console.log(fish[fishName])
+  }
+
+
+export {}
