681775f097b7232ac54da92eb77958f37771d7ab
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-07-11 22:31:43 +0900

    コード整形

--- typescript-space/src/indexed-access.ts
+++ typescript-space/src/indexed-access.ts
@@ -1,15 +1,13 @@
+type Fish = { name?: string; age?: number }
 
-  type Fish = { name?: string; age?: number }
+let fishName: keyof Fish = "name"
 
-  let fishName: keyof Fish = "name"
+const fish: Fish =
+  Math.random() > 0.5 ? { name: "salmon" } : { name: undefined }
 
-  const fish: Fish =
-    Math.random() > 0.5 ? { name: "salmon" } : { name: undefined }
-
-  if (typeof fish[fishName] !== "undefined") {
-    fish[fishName].toUpperCase() // Object is possibly 'undefined'.ts(2532)
-    console.log(fish[fishName])
-  }
-
+if (typeof fish[fishName] !== "undefined") {
+  fish[fishName].toUpperCase() // Object is possibly 'undefined'.ts(2532)
+  console.log(fish[fishName])
+}
 
 export {}
