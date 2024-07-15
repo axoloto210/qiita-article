0b5aad720071ec0760ee76d8ea80913844f247a0
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-07-15 15:53:52 +0900

    コード例を追加

---
+++ typescript-modules/src/salmon.ts
@@ -0,0 +1,12 @@
+type Fish = {
+  name: string;
+  age: number;
+};
+
+
+const salmon: Fish = {
+  name: "salmon",
+  age: 10,
+};
+
+export {};
---
+++ typescript-modules/src/tuna.ts
@@ -0,0 +1,11 @@
+type Fish = {
+  name: string;
+  age: number;
+};
+
+const fish: Fish = {
+  name: "tuna",
+  age: 5,
+};
+
+export {};
