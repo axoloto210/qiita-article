b5a1daa082e69d6fc2ecd30bf190830871b70f51
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2024-12-22 04:47:23 +0900

    add example

---
+++ example/generator/index.ts
@@ -0,0 +1,27 @@
+const iterableObject = {
+    *[Symbol.iterator]() {
+        const random = Math.floor(Math.random() * 10) 
+        let restNum = random
+        
+        while (restNum < 10) {
+            yield restNum++
+        }
+    }
+}
+
+console.log([...iterableObject])
+
+
+
+// const iterableObject = {
+//     *generatorFn() {
+//         const random = Math.floor(Math.random() * 10) 
+//         let restNum = random
+        
+//         while (restNum < 10) {
+//             yield restNum++
+//         }
+//     }
+// }
+
+// console.log([...iterableObject.generatorFn()])
